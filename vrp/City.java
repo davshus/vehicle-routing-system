5960b44e36e910f8022fd1a242bef2563d2d00d7
Alden Lamp <alamp9008@gmail.com>
2017-04-17 12:13:34 -0400

    starting algorithms

--- vrp/City.java
+++ vrp/City.java
@@ -2,22 +2,34 @@ package vrp;
 import java.io.File;
 import java.util.Scanner;
 import vrp.Pair;
-// import vrp.Avenue;
+import vrp.Avenue;
+import tinshoes.geom.Point;
 public class City {
-	public static int nStreets = 250, nAvenues = 50;
-	Pair[][] map;
+	Avenue[][] map;
 	public City() {
-		map = new Pair[nStreets][nAvenues * 10];
 	}
 	public void setupMap(File inputFile) throws Exception {
-		//Bart Complex
 		Scanner sc = new Scanner(inputFile).useDelimiter(",|\r?\n\r?|\n?\r\n?");
 		while (sc.hasNext()) {
 			System.out.println(sc.next());
 		}
 
+
 	}
-	public Pair[][] getMap() {
+	public Avenue[][] getMap() {
 		return this.map;
 	}
+
+
+	public getNextTo(int x, int y){
+
+	}
+
+	public Pair floodFill(int x, int y){
+		Pair startPoint = map[x][y];
+
+
+			
+	}
+
 }
