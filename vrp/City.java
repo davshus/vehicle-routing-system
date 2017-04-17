91ffae615ff47b08c19fe15d2b3e7b234dac5c93
David Shustin <david.shustin@gmail.com>
2017-04-17 12:12:18 -0400

    Remove Avenue References in City.java

--- vrp/City.java
+++ vrp/City.java
@@ -2,20 +2,22 @@ package vrp;
 import java.io.File;
 import java.util.Scanner;
 import vrp.Pair;
-import vrp.Avenue;
+// import vrp.Avenue;
 public class City {
-	Avenue[][] map;
+	public static int nStreets = 250, nAvenues = 50;
+	Pair[][] map;
 	public City() {
+		map = new Pair[nStreets][nAvenues * 10];
 	}
 	public void setupMap(File inputFile) throws Exception {
+		//Bart Complex
 		Scanner sc = new Scanner(inputFile).useDelimiter(",|\r?\n\r?|\n?\r\n?");
 		while (sc.hasNext()) {
 			System.out.println(sc.next());
 		}
 
-
 	}
-	public Avenue[][] getMap() {
+	public Pair[][] getMap() {
 		return this.map;
 	}
 }
