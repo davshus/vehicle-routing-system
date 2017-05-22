b31e5021d89decedf6e45fa646c33a9f13227d84
Alden Lamp <alamp9008@gmail.com>
2017-05-22 12:13:50 -0400

    HELP ME

--- vrp/Main.java
+++ vrp/Main.java
@@ -11,10 +11,60 @@ public class Main {
 
 
 	public static void main(String[] args) {
-		for (int i = 1; i < 12; i++){
+
+
+		int[] trucks = new int[11];
+		double[] time = new double[11];
+		double[] distance = new double[11];
+		int[] packages = new int[11];
+		ArrayList<ArrayList<Integer>> distPerClust = new ArrayList<ArrayList<Integer>>();
+
+
+
+///OUT OF 12
+		for (int i = 1; i < 11; i++){
 			returnStatement r = vrp("cycle" + i + ".txt");
-			System.out.println("Cycle: " + i + "\tTrucks: " + r.getTrucks() + "\tTime: " + r.getTime() + "\tDistance: " + r.getDistance() + "\tPackages: " + r.getPackages());
+			trucks[i-1] = r.getTrucks();
+			time[i-1] = r.getTime();
+			distance[i-1] = r.getDistance();
+			packages[i-1] = r.getPackages();
+			distPerClust.add(r.getClusterDistance());
+			System.out.println("Calculating: \t" + i);
 		}
+
+		int[] tempTrucks = trucks;
+		Arrays.sort(tempTrucks);
+		int median = tempTrucks[tempTrucks.length/2];
+
+		int[] truckDist = new int[median];
+
+		int totalCost = 0;
+
+		for (int i = 0; i < 11; i++){
+
+			int cost = 0;
+
+			for (int dist = truckDist.length; dist > 0; dist--){
+				int miles = truckDist[dist-1]/5000;
+				if (miles > 100){
+					truckDist[dist-1] -= 500000;
+					cost += 1000;
+				}
+				ArrayList<Integer> distPerCluster = distPerClust.get(i-1);
+				truckDist[dist] += distPerCluster.get(dist);
+				cost += miles * 5;
+			}
+			if (trucks[i] > median){
+				cost += 15000 * (trucks[i] - median);
+			}
+			totalCost += cost;
+			System.out.println("Cycle: " + (i + 1) + "\tTrucks: " + trucks[i] + "\tTime: " + time[i] + "\tDistance: " + distance[i] + "\tPackages: " + packages[i] + "\tCost: " + cost);
+		}
+
+
+
+
+
 
 	}
 
@@ -37,13 +87,14 @@ public class Main {
 		double gTime = -1;
 		int totalDistance = 0;
 		int gPackages = 0;
+		ArrayList<Integer> distClust;
 		do {
 			hv.resetDeliveries();
 			nTrucks++;
 			totalDistance = 0;
 			gPackages = 0;
 			int[][] kMeanPoints = hv.kMeans(startPoint, nTrucks);
-
+			distClust = new ArrayList<Integer>();
 			boolean start = true;
 
 			ArrayList<ArrayList<Path>> routes = new ArrayList<ArrayList<Path>>(); //Not using arrays because generic arrays are not supported...
@@ -55,6 +106,7 @@ public class Main {
 				for (Path p : routes.get(i)) {
 					totalDistance += p.getDistance();
 					distance += p.getDistance();
+					distClust.add(p.getDistance());
 					packages += p.getEnd().getDeliver();
 				}
 				double time = (packages * 60) + ((distance/100) * 3);
@@ -64,49 +116,14 @@ public class Main {
 					totalDistance = distance;
 				}
 			}
-
-
-
-			// clear();
-			// for (ArrayList<Path> a : routes){
-			// 	for (Path f : a){
-			// 		write(f.getEnd().toString() + "\n");
-			// 	}
-			// 	write("\n\n");
-			// }
-
-			// for (Pair[] a : hv.getMap()){
-			// 	for(Pair f : a){
-			// 		if (f.getDeliver() > 0){
-			// 			write(f.toString() + "\t" + f.getCluster() + "\t" + (f.delivered() ? "DELIVERED" : "NOT DELIVERED") + "\t" + (City.calcY(f) % 2 == 0 ? "EVEN" : "ODD") + "\t" + f.getDeliver() + "\n");
-
-			// 		}
-			// 	}
-			// }
-
-			// for(int z = 249; z >= 0; z--){
-			// 		for (int j = 449; j >= 0; j--){
-			// 			int f = 0;
-			// 			for (int[] k : kMeanPoints){
-			// 				if (k[0] == z && k[1] == j){
-			// 					write((f < 10 ? f : Character.toString((char)((f - 10) + (int)'a'))) + "*");
-			// 				}
-			// 				f ++;
-			// 			}
-			// 			write((hv.getMap()[z][j].getCluster() == -1) ? " " : (hv.getMap()[z][j].getCluster() < 10 ? Integer.toString(hv.getMap()[z][j].getCluster()) : Character.toString((char)((hv.getMap()[z][j].getCluster() - 10) + (int)'a'))));
-			// 		}
-			// 		write("\n");
-			// 	}
-
-			// System.out.println(gTime/3600);
 		} while (gTime/3600 > 24); // seconds / 3600 = hours
 		writer.close();
 
-		
+
 		// System.out.println("The total cost is:\tto be determined");
 
 
-		returnStatement r = new returnStatement(nTrucks, hv.totalPackages, round(gTime/3600, 4), totalDistance);
+		returnStatement r = new returnStatement(nTrucks, hv.totalPackages, round(gTime/3600, 4), totalDistance, distClust);
 		return r;
 	}
 
@@ -118,9 +135,6 @@ public class Main {
     	long tmp = Math.round(value);
     	return (double) tmp / factor;
 	}
-
-
-
 
 	public static void clear(){
 
@@ -140,14 +154,16 @@ public class Main {
 
 class returnStatement{
 
-	private int trucks, packages;
-	private double time, distance;
+	private int trucks, packages, distance;
+	private double time;
+	private ArrayList<Integer> clusterDistance;
 
-	public returnStatement(int trucks, int packages, double time, double distance){
+	public returnStatement(int trucks, int packages, double time, int distance, ArrayList<Integer> clusterDistance){
 		this.trucks = trucks;
 		this.packages = packages;
 		this.time = time;
 		this.distance = distance;
+		this.clusterDistance = clusterDistance;
 	}
 
 	public returnStatement(String error){ System.out.println(error); }
@@ -155,6 +171,6 @@ class returnStatement{
 	public int getTrucks(){ return this.trucks; }
 	public int getPackages(){ return this.packages; }
 	public double getTime(){ return this.time; }
-	public double getDistance(){ return this.distance; }
+	public int getDistance(){ return this.distance; }
+	public ArrayList<Integer> getClusterDistance(){ return this.clusterDistance; }
 }
-
